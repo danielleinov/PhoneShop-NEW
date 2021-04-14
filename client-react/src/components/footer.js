69e3a37e8d7a7005c32256f2000409a7b5cb17bb
aloneh <aloneh@microsoft.com>
2021-04-14 17:55:12 +0300

    Working

--- client-react/src/components/footer.js
+++ client-react/src/components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="py-5 bg-dark">
+        <footer className="py-5 bg-dark footer">
             <div className="container">
                 <p className="m-0 text-center text-white">Copyright © Your Website 2020</p>
             </div>
