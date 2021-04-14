e19bf1a7c4b16c0f05fae6d99e7554784fd79337
aloneh <aloneh@microsoft.com>
2021-04-14 17:28:57 +0300

    Fix reviews page design

--- client-react/src/shop/details.js
+++ client-react/src/shop/details.js
@@ -1,6 +1,11 @@
 import React,{useEffect, useState} from "react";
 import axios from "axios";
+import './details.css';
 
+const SUBMITED_STATUS = {
+    NOT_SUBMITED: 'not-submited',
+    SUBMITED_SUCCESSFULLY: 'submited-successfully'
+}
 
 export default function Details() {
 
@@ -9,8 +14,8 @@ export default function Details() {
     const [phoneDetails, setPhoneDetails] = React.useState(null);
     const [reviewContent, setReviewContent] = useState("");
     const [user, setUser] = useState();
-
-
+    const [loading, setLoading] = useState(false);
+    const [submitedStatus, setSubmitedStatus] = useState(SUBMITED_STATUS.NOT_SUBMITED);
 
     React.useEffect(() => {
         const index = window.location.toString().lastIndexOf('/') + 1;
@@ -27,22 +32,39 @@ export default function Details() {
             .then((data) => setPhoneDetails(data));
     }, []);
 
-    const SubmitReview = async e => {
+    React.useEffect(() => {
+        if(submitedStatus === SUBMITED_STATUS.SUBMITED_SUCCESSFULLY)
+        {
+            const index = window.location.toString().lastIndexOf('/') + 1;
+            const id = window.location.toString().substring(index);
 
+            fetch('http://localhost:8080/api/phone/' + id)
+                .then((response) => response.json())
+                .then((data) => setPhoneDetails(data));
+            setSubmitedStatus(SUBMITED_STATUS.NOT_SUBMITED);
+            setLoading(false)
+        }
+    }, [submitedStatus]);
+
+    const SubmitReview = async e => {
+        setLoading(true)
         const review = {phoneId: phoneId, content: reviewContent, author: user.name };
         const response = await axios.post(
             "http://localhost:8080/api/review/",
             review
         );
-        // Refresh page
+        setSubmitedStatus(SUBMITED_STATUS.SUBMITED_SUCCESSFULLY);
     }
 
     if (phoneDetails === null)
         return "";
 
     return (
-        <div className="col-lg-9">
-            <div className="card mt-4">
+        <div className="root">
+            <div className="loader-container" >
+                <div className={loading ? "loader" : ""}></div>
+            </div>
+            <div className="card mt-4 mr-4" >
                 <img className="card-img-top img-fluid" src="http://placehold.it/900x400" alt="" />
                 <div className="card-body">
                     <h3 className="card-title">{phoneDetails.displayName}</h3>
@@ -54,25 +76,25 @@ export default function Details() {
                 </div>
             </div>
             {/* /.card */}
-            <div className="card card-outline-secondary my-4">
+            <div className="card card-outline-secondary my-4" >
                 <div className="card-header">
                     Product Reviews
                 </div>
-                <div className="card-body">
+                <div className="card-body overflow-auto">
                     {
-                        phoneDetails.reviews.map((data, key) => {
-                            return ([
-                                <p key={key}>{data.content}</p>,
-                                <small className="text-muted">Posted by {data.author} on {data.date}</small>,
-                                <hr />,
-                            ]
+                        phoneDetails.reviews.map((data, index) => {
+                            return (
+                                <div key={index}>
+                                    <p>{data.content}</p>
+                                    <small className="text-muted">Posted by {data.author} on {data.date}</small>
+                                    <hr />
+                                </div>
                             )
                         })
-
                     }
-                    <input type="text" name="name" value={reviewContent} placeholder="Write a review" onChange={({target}) => setReviewContent(target.value)}/>
-                    <button onClick={SubmitReview} className="btn btn-success">Leave a Review</button>
                 </div>
+                <input type="text" name="name" value={reviewContent} placeholder="Write a review" onChange={({target}) => setReviewContent(target.value)}/>
+                <button onClick={SubmitReview} className="btn btn-success">Leave a Review</button>
             </div>
         </div>
     );
