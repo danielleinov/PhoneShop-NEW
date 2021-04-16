11aeac397378ece7d45eb4c5f18ce1a54bf11c0b
Daniel Vaknin <danielvaknin10@gmail.com>
2021-04-16 18:46:16 +0300

    Add routes for reviews (get, update, delete)

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -2,12 +2,12 @@ const Review = require('../models/review');
 const Phone = require('../models/phone');
 
 module.exports.addReview = async (req, res, next) => {
-    const { phoneId, content, author } = req.body;
+    const {phoneId, content, author} = req.body;
     const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
     console.log(date)
-    const review = new Review({ content, author, date });
+    const review = new Review({content, author, date});
     await review.save();
-    Phone.findOne({ _id: phoneId }).then(function(phone) {
+    Phone.findOne({_id: phoneId}).then(function (phone) {
 
         phone.reviews.push(review);
         phone.save();
@@ -23,4 +23,31 @@ module.exports.getReviews = async (req, res) => {
 module.exports.getReviewsByPhoneId = async (req, res) => {
     const reviews = await Phone.findById(req.params.id).populate("reviews")
     res.send(reviews)
+}
+
+module.exports.getReviewById = async (req, res) => {
+    const {id} = req.params;
+    Review.findById(id, function (err, doc) {
+        if (err) return res.send(500, {error: err});
+        return res.send(doc);
+    });
+}
+
+module.exports.updateReviewById = async (req, res) => {
+    const {id} = req.params;
+    Review.findOneAndUpdate({_id: id}, req.body, {upsert: true}, function (err, doc) {
+        if (err) return res.send(500, {error: err});
+        return res.send(doc);
+    });
+}
+
+module.exports.deleteReviewById = async (req, res) => {
+    const {id} = req.params;
+    Review.findByIdAndDelete(id, (err, doc) => {
+        if (err) res.send(err)
+        res.json({
+            "status": "ok",
+            "Deleted": doc
+        });
+    });
 }
