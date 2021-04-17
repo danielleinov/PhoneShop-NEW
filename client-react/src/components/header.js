f0a1b4b10acfe203da671ec9c709cc60a1dad856
Daniel Vaknin <danielvaknin10@gmail.com>
2021-04-17 16:06:13 +0300

    Hide shopping cart when not logged in

--- client-react/src/components/header.js
+++ client-react/src/components/header.js
@@ -1,16 +1,14 @@
 import React, {useEffect} from "react";
-import {Link, Redirect, useHistory} from "react-router-dom";
+import {Link} from "react-router-dom";
 import "./header.css";
 
 export default function Header({count, onCountChange}) {
-
-    const history = useHistory();
 
     const onLogout = (e) => {
         localStorage.removeItem('user');
     }
 
-    const route = JSON.parse(localStorage.getItem("cart")) ? `/cart/${JSON.parse(localStorage.getItem("cart"))._id}`: ""
+    const route = JSON.parse(localStorage.getItem("cart")) ? `/cart/${JSON.parse(localStorage.getItem("cart"))._id}` : ""
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
@@ -23,19 +21,24 @@ export default function Header({count, onCountChange}) {
                                 <span className="sr-only">(current)</span>
                             </Link>
                         </li>
-                        <li className="nav-item active">
-                            <Link className="nav-link" to={route}>
-                                <svg width="18px"  fill="currentColor" viewBox="0 0 20 20"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg>
-                                ({count})
-                                <span className="sr-only">(current)</span>
-                            </Link>
-                        </li>
-                        { !!localStorage.getItem('user') ?
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/" onClick={onLogout}>Logout
-                                    <span className="sr-only">(current)</span>
-                                </Link>
-                            </li>
+                        {!!localStorage.getItem('user') ?
+                            <>
+                                <li className="nav-item active">
+                                    <Link className="nav-link" to={route}>
+                                        <svg width="18px" fill="currentColor" viewBox="0 0 20 20">
+                                            <path
+    d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"/>
+                                        </svg>
+                                        ({count})
+                                        <span className="sr-only">(current)</span>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/" onClick={onLogout}>Logout
+                                        <span className="sr-only">(current)</span>
+                                    </Link>
+                                </li>
+                            </>
                             :
                             <li className="nav-item">
                                 <Link className="nav-link" to="/login">Login
@@ -43,16 +46,6 @@ export default function Header({count, onCountChange}) {
                                 </Link>
                             </li>
                         }
-                        {/*<li className="nav-item">*/}
-                        {/*    <Link className="nav-link" to="/login">Login*/}
-                        {/*        <span className="sr-only">(current)</span>*/}
-                        {/*    </Link>*/}
-                        {/*</li>*/}
-                        {/*<li className="nav-item">*/}
-                        {/*    <Link className="nav-link" to="/logout">Logout*/}
-                        {/*        <span className="sr-only">(current)</span>*/}
-                        {/*    </Link>*/}
-                        {/*</li>*/}
                     </ul>
                 </div>
             </div>
