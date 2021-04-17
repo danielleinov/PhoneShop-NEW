9fb846bc808606496a92555bcd8d7baed25eb282
daniel <daniel@airsoftltd.com>
2021-04-17 15:14:10 +0300

    fix add to cart in search

--- client-react/src/components/search.js
+++ client-react/src/components/search.js
@@ -1,12 +1,12 @@
 import List from "../shop/list";
 
-export default function Search() {
+export default function Search({count, onCountChange}) {
 
     const q = window.location.search.toString();
 
     return (
         <div className="col-lg-9">
-            <List q={'/search' + q}/>
+            <List q={'/search' + q} count={count} onCountChange={onCountChange}/>
         </div>
     );
 }
